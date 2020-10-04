a42c88a5f76ea7199142ef0348cc0e72690b1e2f
YairAriel <yair009@gmail.com>
2020-10-04 23:11:03 +0300

    Updates

---
+++ precache-manifest.a6e830588a463d9be94b23b9ab8aea00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b4600864e2e1c44eb4e719d18f2bc5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36c03c69d171bd044611",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "be94b6ee64ec6a85d420",
+    "url": "./static/js/2.9000f436.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9000f436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c03c69d171bd044611",
+    "url": "./static/js/main.f0e2e3df.chunk.js"
+  },
+  {
+    "revision": "75d81265ec4e205e8dd8",
+    "url": "./static/js/runtime-main.168119b4.js"
+  }
+]);
