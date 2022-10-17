e58efabb3e8ebd496cbc429109e29f25da717250
Yair Ariel <yair.ariel@jgive.com>
2022-10-18 00:09:05 +0300

    Updates

---
+++ precache-manifest.32d009233191093f761c88b506d47513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d445ded28c0ef67cc2717fccc3f08246",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56f15328991bfd7e71ff",
+    "url": "./static/css/main.53559c22.chunk.css"
+  },
+  {
+    "revision": "c8a12f23982a9ba035a2",
+    "url": "./static/js/2.4f7a156b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.4f7a156b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f15328991bfd7e71ff",
+    "url": "./static/js/main.542c3ed4.chunk.js"
+  },
+  {
+    "revision": "c221c968a5aee6c11483",
+    "url": "./static/js/runtime-main.951177be.js"
+  }
+]);
