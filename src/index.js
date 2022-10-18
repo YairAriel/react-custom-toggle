627a3c1a26986ce8a5ea39d0dd21fb0145d8a1a4
Yair Ariel <yair009@gmail.com>
2022-10-18 23:03:49 +0300

    Formatting the Toggle component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
+/** @format */
+
+import React from 'react';
+import styled from 'styled-components';
 
 const Toggle = ({
   checked,
@@ -14,9 +16,9 @@ const Toggle = ({
   colorOff,
   textColorOff,
   textColorOn,
-  disabled
+  disabled,
 }) => (
-  <Container>
+  <ToggleContainer>
     {offText && (
       <OffText
         extraLarge={extraLarge}
@@ -28,14 +30,9 @@ const Toggle = ({
         {offText}
       </OffText>
     )}
-    <Wrapper
-      extraLarge={extraLarge}
-      large={large}
-      small={small}
-      extraSmall={extraSmall}
-    >
+    <Wrapper extraLarge={extraLarge} large={large} small={small} extraSmall={extraSmall}>
       <Input
-        type='checkbox'
+        type="checkbox"
         checked={checked}
         onChange={(e) => onChange(e.target.checked)}
         disabled={disabled}
@@ -65,14 +62,14 @@ const Toggle = ({
         {onText}
       </OnText>
     )}
-  </Container>
-)
+  </ToggleContainer>
+);
 
-const Container = styled.div`
+const ToggleContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const Text = styled.span`
   font-family: inherit;
@@ -80,34 +77,34 @@ const Text = styled.span`
     if (props.extraLarge) {
       return `
         font-size: 1.4em;
-      `
+      `;
     } else if (props.large) {
       return `
         font-size: 1.2em;
-      `
+      `;
     } else if (props.small) {
       return `
         font-size: 0.8em;
-      `
+      `;
     } else if (props.extraSmall) {
       return `
         font-size: 0.6em;
-      `
+      `;
     } else {
       return `
         font-size: 1em;
-      `
+      `;
     }
   }}
-`
+`;
 
 const OffText = styled(Text)`
   color: ${(props) => (props.textColorOff ? props.textColorOff : 'inherit')};
-`
+`;
 
 const OnText = styled(Text)`
   color: ${(props) => (props.textColorOn ? props.textColorOn : 'inherit')};
-`
+`;
 
 const Wrapper = styled.label`
   position: relative;
@@ -118,30 +115,30 @@ const Wrapper = styled.label`
       return `
         width: 100px;
         height: 50px;
-      `
+      `;
     } else if (props.large) {
       return `
         width: 80px;
         height: 40px;
-      `
+      `;
     } else if (props.small) {
       return `
         width: 40px;
         height: 20px;
-      `
+      `;
     } else if (props.extraSmall) {
       return `
         width: 20px;
         height: 10px;
-      `
+      `;
     } else {
       return `
         width: 60px;
         height: 30px;
-      `
+      `;
     }
   }}
-`
+`;
 
 const Input = styled.input`
   display: none;
@@ -152,28 +149,28 @@ const Input = styled.input`
         if (props.extraLarge) {
           return `
             transform: translateX(48px) rotateZ(360deg);
-        `
+        `;
         } else if (props.large) {
           return `
             transform: translateX(38px) rotateZ(360deg);
-        `
+        `;
         } else if (props.small) {
           return `
             transform: translateX(18px) rotateZ(360deg);
-        `
+        `;
         } else if (props.extraSmall) {
           return `
             transform: translateX(8px) rotateZ(360deg);
-        `
+        `;
         } else {
           return `
             transform: translateX(28px) rotateZ(360deg);
-        `
+        `;
         }
       }}
     }
   }
-`
+`;
 
 const Slider = styled.span`
   position: absolute;
@@ -185,8 +182,7 @@ const Slider = styled.span`
   cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
   background: ${(props) =>
     props.colorOff ? props.colorOff : props.disabled ? '#d2d2d2' : '#fc8181'};
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2),
-    inset 0 3px 8px 0 rgba(0, 0, 0, 0.3);
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2), inset 0 3px 8px 0 rgba(0, 0, 0, 0.3);
   border-radius: 36px;
   transition: transform 300ms, background-color 300ms;
   transition-delay: 200ms;
@@ -194,23 +190,23 @@ const Slider = styled.span`
     if (props.extraLarge) {
       return `
         padding-inline-start: 6px;
-      `
+      `;
     } else if (props.large) {
       return `
         padding-inline-start: 5px;
-      `
+      `;
     } else if (props.small) {
       return `
         padding-inline-start: 3px;
-      `
+      `;
     } else if (props.extraSmall) {
       return `
         padding-inline-start: 2px;
-      `
+      `;
     } else {
       return `
         padding-inline-start: 4px;
-      `
+      `;
     }
   }}
   &::before {
@@ -225,34 +221,34 @@ const Slider = styled.span`
           width: 40px;
           height: 40px;
           top: 5px;
-      `
+      `;
       } else if (props.large) {
         return `
           width: 32px;
           height: 32px;
           top: 4px;
-      `
+      `;
       } else if (props.small) {
         return `
           width: 16px;
           height: 16px;
           top: 2px;
-      `
+      `;
       } else if (props.extraSmall) {
         return `
           width: 8px;
           height: 8px;
           top: 1px;
-      `
+      `;
       } else {
         return `
           width: 24px;
           height: 24px;
           top: 3px;
-      `
+      `;
       }
     }}
   }
-`
+`;
 
-export default Toggle
+export default Toggle;
